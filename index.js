1451910a63b6051ad08ca92c6b58a49d75dbc063
diegorodd <dieegoa@icloud.com>
2022-07-17 19:18:16 -0400

    more files and index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const inquirer = require('inquirer');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const generatePage = require('./src/page-Template');
+const fs = require('fs');
+
+const employeesArray = [];
+
